5caf0eda0210d62664003d2c69daba5c9620ee6c
parusha <parusha.pradhan@gmail.com>
2025-04-15 20:43:23 -0400

    remove output flush

--- hw4lib/model/masks.py
+++ hw4lib/model/masks.py
@@ -26,12 +26,8 @@ def PadMask(padded_input, input_lengths):
             - non-padding positions are marked with False.
     """
     # TODO: Implement PadMask
-    # T = padded_input.shape[1]
-    # print('input_lengths', input_lengths)
-    # mask = torch.arange(T, device=padded_input.device).unsqueeze(0) >= input_lengths.unsqueeze(1)
 
     N, T = padded_input.shape[:2]
-    print("input_length: ", input_lengths, flush=True)
     range_tensor = torch.arange(T, device=padded_input.device).expand(N, T)
     mask = range_tensor >= input_lengths.unsqueeze(1)
 
